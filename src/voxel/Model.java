83275d745b6466aef8942a9080a14510aeb47f98
Ergoold <eran1gold@gmail.com>
2021-06-17 02:23:46 +0300

    Create Model

---
+++ src/voxel/Model.java
@@ -0,0 +1,32 @@
+package voxel;
+
+/**
+ * A 3d voxel model.
+ * <p>
+ * There are three axes: x / width, y / height, z / depth.
+ */
+public class Model {
+
+    /**
+     * Which voxels are present and which are missing.
+     * <p>
+     * The x and y axes are switched.
+     */
+    private final boolean[][][] voxels;
+
+    public Model(int width, int height, int depth) {
+        voxels = new boolean[height][width][depth];
+    }
+
+    public void addVoxel(int x, int y, int z) {
+        voxels[y][x][z] = true;
+    }
+
+    public boolean isVoxelPresent(int x, int y, int z) {
+        return voxels[y][x][z];
+    }
+
+    public boolean[][] getLayer(int layer) {
+        return voxels[layer];
+    }
+}
